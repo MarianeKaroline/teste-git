6c783fefe4057c706e3e2dc29d79c0d2d93558ac
MarianeKaroline <nani_karoline@hotmail.com>
2021-07-21 13:46:30 -0300

    Projeto, calculando se aluno foi aprovado

---
+++ ExercicioNotaAluno/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioNotaAluno
+{
+    class Aluno
+    {
+        public string Nome;
+        public double[] Notas = new double[3];
+        public double MediaFinal;
+
+        public double NotaFinal()
+        {
+            MediaFinal = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                MediaFinal += Notas[i];
+            }
+            return MediaFinal;
+        }
+
+        public string Aprovado()
+        {
+            if (MediaFinal > 60.0)
+            {
+                return "Aprovado";
+            }
+            else
+            {
+                return "Reprovado";
+            }
+        }
+
+        public double PontosFaltantes()
+        {
+            return 60.0 - MediaFinal;
+        }
+    }
+}
---
+++ ExercicioNotaAluno/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioNotaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.Write("Digite as três notas do aluno: ");
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                aluno.Notas[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("\nNota final = " + aluno.NotaFinal());
+            Console.WriteLine("Situação " + aluno.Aprovado());
+
+            if (aluno.Aprovado() == "Reprovado")
+            {
+                Console.WriteLine("Faltaram " + aluno.PontosFaltantes().ToString("F2", CultureInfo.InvariantCulture) + " pontos");
+            }
+        }
+    }
+}
